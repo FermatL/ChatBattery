bf3b0c25347df78e53257805132f5a14c363d5a2
LI LINGYI <166478134+FermatL@users.noreply.github.com>
2025-07-29 17:04:09 +0800

    retrieval_agent.py

--- ChatBattery/retrieval_agent.py
+++ ChatBattery/retrieval_agent.py
@@ -4,13 +4,15 @@ from .domain_agent import Domain_Agent
 
 class Retrieval_Agent:
     @staticmethod
-    def retrieve_with_domain_feedback(task_index, DB, input_battery, generated_battery):
+    def retrieve_with_domain_feedback(DB, input_battery, generated_battery):
         distance_DB = DB.copy()
         distance_list = []
         for index, row in distance_DB.iterrows():
             formula = row['formula']
-            distance = Domain_Agent.distance_function(task_index, formula, generated_battery)
+            # task_index 已从 distance_function 中移除
+            distance = Domain_Agent.distance_function(formula, generated_battery)
             distance_list.append(distance)
+        
         distance_DB['distance'] = distance_list
         
         distance_DB = distance_DB.sort_values(by=['distance'], ascending=True)
@@ -24,11 +26,11 @@ class Retrieval_Agent:
             retrieved_battery = row['formula']
             if generated_battery == retrieved_battery:
                 continue
-            input_value, output_value, answer = Decision_Agent.decide_one_pair(input_battery, retrieved_battery, task_index)
+            # task_index 已从 decide_one_pair 中移除
+            input_value, output_value, answer = Decision_Agent.decide_one_pair(input_battery, retrieved_battery)
             print("input battery: {} ({:.3f})\tretrieved battery: {} ({:.3f})".format(input_battery, input_value, retrieved_battery, output_value))
 
             if answer:
                 print()
                 return retrieved_battery, output_value
         raise Exception("Sorry, cannot fined a good formula.")
-
